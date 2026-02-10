cac58521ad88927eb12c16c21d9cc1769aa001c3
yakisabadesho <assyl.aubanov@gmail.com>
2026-02-10 12:58:16 +0500

    Create chapter1.js

---
+++ Lab3/task1/chapter1.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Website</title>
+  <style>
+
+  </style>
+</head>
+<body>
+
+<script>
+  window.alert("俺はジャヴァスクリプト！");
+</script>
+
+</body>
+</html>
